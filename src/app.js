e190eb679c58b2d5149e44404b6f76076d79711c
Mario Melchor <mariomelchor@M2-MacBook.attlocal.net>
2018-07-04 22:16:18 -0700

    Added react-redux, Setup Provider, Rendering Expense List and filter inputs

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouters';
 import configureStore from './store/configureStore';
 import { addExpense } from './actions/expenses';
@@ -10,8 +11,9 @@ import './styles/styles.scss';
 
 const store = configureStore();
 
-store.dispatch( addExpense( { description: 'water bill' } ));
-store.dispatch( addExpense( { description: 'gas bill' } ));
+store.dispatch( addExpense( { description: 'water bill', amount: 4500 } ));
+store.dispatch( addExpense( { description: 'gas bill', amount: 5500, createdAt: 1000 } ));
+store.dispatch( addExpense( { description: 'Rent bill', amount: 100500 } ));
 store.dispatch( setTextFilter( 'bill' ));
 
 const state = store.getState();
@@ -20,4 +22,10 @@ console.log(visibleExpenses);
 
 console.log(store.getState());
 
-ReactDOM.render( <AppRouter />, document.getElementById('app') );
+const jsx = (
+  <Provider store={store}>
+    <AppRouter />
+  </Provider>
+)
+
+ReactDOM.render( jsx, document.getElementById('app') );
